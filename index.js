6b26b918458d43090031253fc6eee9c3731fbb69
monaemhossain <monaemhs@gmail.com>
2023-10-20 15:54:26 +0300

    added user path for user details

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-// username monaemFashion
-// pass 2DHhpPadK8XVuafw
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xt3dv3d.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -37,16 +34,34 @@ async function run() {
         const result = await cursor.toArray();
         res.send(result);
     })
-
-
-    app.post('/products', async(req, res) => {
-        const newProduct = req.body;
-        console.log(newProduct);
-        const result = await productsDatabase.insertOne(newProduct);
+    
+    app.get('/user', async(req, res) => {
+        const cursor = productsDatabase.find();
+        const result = await cursor.toArray();
         res.send(result);
     })
 
 
+
+
+
+
+
+    app.post('/products', async(req, res) => {
+        const newProduct = req.body;
+        // console.log(newProduct);
+        const result = await productsDatabase.insertOne(newProduct);
+        res.send(result);
+    })
+
+    
+    app.post('/user', async(req, res) => {
+        const newProduct = req.body;
+        // console.log(newProduct);
+        const result = await productsDatabase.insertOne(newProduct);
+        res.send(result);
+    })
+    
 
 
 
