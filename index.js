d437522af96516213723deb8211ad962ad1fdc3f
monaemhossain <monaemhs@gmail.com>
2023-10-21 08:09:35 +0300

    added fucntionality to add user to the database

--- index.js
+++ index.js
@@ -14,71 +14,74 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    const productsDatabase = client.db('productsDB').collection('products');
+        const productsDatabase = client.db('productsDB').collection('products');
+        const userDatabase = client.db('userDB').collection('users');
 
-    
-    app.get('/products', async(req, res) => {
-        const cursor = productsDatabase.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
-    
-    app.get('/user', async(req, res) => {
-        const cursor = productsDatabase.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+        // Read products
+        app.get('/products', async (req, res) => {
+            const cursor = productsDatabase.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
-
-
-
+        // Read users
+        app.get('/users', async (req, res) => {
+            const cursor = userDatabase.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
 
 
-    app.post('/products', async(req, res) => {
-        const newProduct = req.body;
-        // console.log(newProduct);
-        const result = await productsDatabase.insertOne(newProduct);
-        res.send(result);
-    })
-
-    
-    app.post('/user', async(req, res) => {
-        const newProduct = req.body;
-        // console.log(newProduct);
-        const result = await productsDatabase.insertOne(newProduct);
-        res.send(result);
-    })
-    
 
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        // product api
+        app.post('/products', async (req, res) => {
+            const newProduct = req.body;
+            // console.log(newProduct);
+            const result = await productsDatabase.insertOne(newProduct);
+            res.send(result);
+        })
+
+        // user api
+        app.post('/users', async (req, res) => {
+            const newUser = req.body;
+            // console.log(newUser);
+            const result = await userDatabase.insertOne(newUser);
+            res.send(result);
+        })
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('server running')
 })
 
